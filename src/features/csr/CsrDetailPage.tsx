b8d52c7ee746d7b197c8d52eba02deddaa2ad46a
kimaresen <sourla@daum.net>
2026-09-01 23:19:01 +0900

    Add CSR post detail page with dynamic data loading using useEffect

---
+++ src/features/csr/CsrDetailPage.tsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { fetchPost } from '../../common/api/posts'
+import { PostDetail } from '../../common/components/PostDetail'
+import type { Post } from '../../common/types/post'
+export function CsrDetailPage() {
+  const { id } = useParams()
+  const [post, setPost] = useState<Post>()
+  useEffect(() => {
+    if (id) fetchPost(Number(id)).then(setPost)
+  }, [id])
+  return post ? (
+    <PostDetail post={post} backPath="/csr" />
+  ) : (
+    <p className="text-zinc-500">게시글을 불러오는 중...</p>
+  )
+}
